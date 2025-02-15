fe9b8dcbca013b65cb47b4c8508bcb535d6ef59a
RRAJ644 <riturajsingh.17248@gmail.com>
2025-02-16 00:09:59 +0530

    h1 fixed

--- components/custom/Hero.tsx
+++ components/custom/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className='min-h-screen flex flex-col items-center justify-center px-6 md:px-12 text-center gap-y-20'>
       <div className='max-w-6xl'>
-        <h1 className='text-5xl md:text-6xl font-extrabold bg-gradient-to-r from-neutral-700 via-zinc-600 to-gray-700 text-transparent bg-clip-text drop-shadow-md'>
+        <h1 className='text-5xl md:text-6xl bg-gradient-to-r from-neutral-700 via-zinc-600 to-gray-700 text-transparent bg-clip-text drop-shadow-md'>
           Discover the Art of Screenwriting and Unlock the Secrets
         </h1>
         <p className='mt-5 text-xl text-gray-700 dark:text-gray-300 leading-relaxed'>
