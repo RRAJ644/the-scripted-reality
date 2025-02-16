c7e7cbad6dc218d5669dbdf41c94dcb3cf548a3a
RRAJ644 <riturajsingh.17248@gmail.com>
2025-02-16 21:17:39 +0530

    Navbar ui fixed

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -8,7 +8,6 @@ import {
   SheetHeader,
   SheetTitle,
 } from '@/components/ui/sheet'
-
 import { Menu, X } from 'lucide-react'
 import Link from 'next/link'
 
@@ -24,11 +23,11 @@ const Navbar = () => {
   ]
 
   return (
-    <nav className='w-full top-0 z-50 shadow-sm text-xl'>
+    <nav className='w-full top-0 z-50 shadow-sm text-xl bg-white dark:bg-gray-900'>
       <div className='max-w-7xl mx-auto px-6 flex justify-between items-center h-16'>
         <Link
           href={'/'}
-          className='text-3xl font-extrabold cursor-pointer tracking-wide'
+          className='text-3xl font-extrabold cursor-pointer tracking-wide text-gray-800 dark:text-white'
         >
           ReadReality
         </Link>
@@ -36,7 +35,12 @@ const Navbar = () => {
         <ul className='hidden md:flex space-x-6'>
           {navLinks.map((link) => (
             <li key={link.name}>
-              <Link href={link.href}>{link.name}</Link>
+              <Link
+                href={link.href}
+                className='hover:text-blue-600 transition-colors'
+              >
+                {link.name}
+              </Link>
             </li>
           ))}
         </ul>
@@ -50,23 +54,26 @@ const Navbar = () => {
                 onClick={() => setOpen(!open)}
               >
                 {open ? (
-                  <X className='h-6 w-6' />
+                  <X className='h-6 w-6 text-gray-800 dark:text-white' />
                 ) : (
-                  <Menu className='h-6 w-6' />
+                  <Menu className='h-6 w-6 text-gray-800 dark:text-white' />
                 )}
               </Button>
             </SheetTrigger>
-            <SheetContent side='right'>
+            <SheetContent side='right' className='text-neutral-900 bg-zinc-50'>
               <SheetHeader className='sr-only'>
-                <SheetTitle>Menu</SheetTitle>
+                <SheetTitle className='text-2xl font-bold mb-6'>
+                  Menu
+                </SheetTitle>
               </SheetHeader>
 
-              <div className='flex flex-col space-y-6 mt-6'>
+              <div className='flex flex-col space-y-6 mt-4 text-xl'>
                 {navLinks.map((link) => (
                   <Link
                     key={link.name}
                     href={link.href}
                     onClick={() => setOpen(false)}
+                    className='hover:text-gray-200 transition-colors'
                   >
                     {link.name}
                   </Link>
