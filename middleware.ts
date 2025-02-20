039974ad4e88acdaf1d7da264171233617f12036
RRAJ644 <riturajsingh.17248@gmail.com>
2025-02-21 01:23:51 +0530

    done route authentication

--- middleware.ts
+++ middleware.ts
@@ -7,14 +7,13 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request })
   const url = request.nextUrl
 
-  // if (
-  //   token &&
-  //   (url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-up'))
-  // ) {
-  //   // return NextResponse.redirect(new URL('/dashboard', request.url))
-  //   // return NextResponse.redirect(new URL('/dashboard', request.url))
+  if (request.nextUrl.pathname === '/sign-up') {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
 
-  // }
+  if (token && url.pathname.startsWith('/sign-in')) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
 
   return NextResponse.next()
 }
