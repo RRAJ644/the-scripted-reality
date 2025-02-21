3a6ea9d823bef93a6c1b46528d068730b50c8570
RRAJ644 <riturajsingh.17248@gmail.com>
2025-02-21 10:46:37 +0530

    dashboard layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import '../globals.css'
+import AuthProvider from '@/context/AuthProvider'
+import Navbar from '@/components/private/Navbar'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'TheScriptedReality - We Write Scripts',
+  description: 'Screenwriters chasing projects with the mind blowing scripts',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <AuthProvider>
+      <html lang='en'>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased text-neutral-800-700 dark:text-zinc-200`}
+        >
+          <Navbar />
+          <main className='min-h-screen'>{children}</main>
+        </body>
+      </html>
+    </AuthProvider>
+  )
+}
