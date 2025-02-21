025102f2c02ba76235eee50042d41015f54d030c
RRAJ644 <riturajsingh.17248@gmail.com>
2025-02-21 15:52:19 +0530

    li fixed

--- components/private/Sidebar.tsx
+++ components/private/Sidebar.tsx
@@ -1,33 +1,59 @@
-import { FileText, PenTool, Archive, Lightbulb, LucideIcon } from 'lucide-react'
-import Link from 'next/link'
+'use client'
+
+import { Button } from '@/components/ui/button'
+import {
+  LucideIcon,
+  FileText,
+  ScrollText,
+  Pencil,
+  Lightbulb,
+} from 'lucide-react'
 
 interface NavLink {
   name: string
-  href: string
+  onClick?: () => void
   icon: LucideIcon
 }
 
 const links: NavLink[] = [
-  { name: 'Blogs', href: '/blogs', icon: FileText },
-  { name: 'Scripts', href: '/scripts', icon: PenTool },
-  { name: 'Drafts', href: '/drafts', icon: Archive },
-  { name: 'Ideas', href: '/ideas', icon: Lightbulb },
+  {
+    name: 'Blogs',
+    onClick: () => console.log('Blogs clicked'),
+    icon: FileText,
+  },
+  {
+    name: 'Scripts',
+    onClick: () => console.log('Scripts clicked'),
+    icon: ScrollText,
+  },
+  {
+    name: 'Drafts',
+    onClick: () => console.log('Drafts clicked'),
+    icon: Pencil,
+  },
+  {
+    name: 'Ideas',
+    onClick: () => console.log('Ideas clicked'),
+    icon: Lightbulb,
+  },
 ]
 
 export default function Sidebar() {
   return (
-    <aside className='w-56 h-screen border-2 text-neutral-900  px-4 py-6 sticky top-0'>
-      <nav className='space-y-3'>
-        {links.map(({ name, href, icon: Icon }) => (
-          <Link
-            key={name}
-            href={href}
-            className='flex items-center gap-3 px-4 py-2 rounded-lg text-neutral-900 transition text-lg'
-          >
-            <Icon size={25} />
-            {name}
-          </Link>
-        ))}
+    <aside className='w-56 h-screen border-2 text-neutral-900 px-4 py-6 sticky top-0'>
+      <nav>
+        <ul className='space-y-4'>
+          {links.map(({ name, onClick, icon: Icon }) => (
+            <li
+              key={name}
+              className='list-none flex items-center justify-center gap-4 text-2xl border-b-2 py-3'
+              role='link'
+            >
+              <Icon size={30} />
+              {name}
+            </li>
+          ))}
+        </ul>
       </nav>
     </aside>
   )
