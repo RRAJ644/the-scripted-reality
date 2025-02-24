576889f85206f1d3c2296d450fa15d6e3657c25b
RRAJ644 <riturajsingh.17248@gmail.com>
2025-02-24 12:48:09 +0530

    tabs for blog editor

--- app/(dashboard)/editor/page.tsx
+++ app/(dashboard)/editor/page.tsx
@@ -10,9 +10,9 @@ const Editor: React.FC = () => {
     <section className='w-full h-full flex flex-col items-center px-6 py-6'>
       <Tabs
         defaultValue='write'
-        className='w-full max-w-xl bg-white border border-gray-200 rounded-xl'
+        className='w-full bg-white border-gray-200 rounded-xl'
       >
-        <TabsList className='flex bg-gray-100 w-full gap-x-2 overflow-hidden h-full rounded-xl'>
+        <TabsList className='flex max-w-3xl gap-x-2 items-center justify-center mx-auto bg-gray-100 overflow-hidden h-full rounded-xl p-2'>
           <TabsTrigger
             value='write'
             className='flex-1 text-gray-700 text-xl border border-gray-400 font-medium data-[state=active]:bg-neutral-800 data-[state=active]:text-white rounded-xl h-full py-2'
