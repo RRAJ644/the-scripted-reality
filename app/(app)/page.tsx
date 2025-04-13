4a1500a12d640420ffdc768ef6c5f1d62ae8a5e0
RRAJ644 <riturajsingh.17248@gmail.com>
2025-04-13 20:10:38 +0530

    trending stories

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero />
       <Featured />
       <Genres />
-      {/* <TrendingStories /> */}
+      <TrendingStories />
       <MediaPartners />
     </section>
   )
