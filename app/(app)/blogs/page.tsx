8eabdcabebad1afcc72542c35f34492d865ea81a
RRAJ644 <riturajsingh.17248@gmail.com>
2025-04-13 14:33:59 +0530

    loader

--- app/(app)/blogs/page.tsx
+++ app/(app)/blogs/page.tsx
@@ -29,7 +29,7 @@ const Blogs = async () => {
         </p>
       </div>
 
-      {blogs.length > 0 && (
+      {blogs?.length > 0 && (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 w-full h-full mt-10'>
           {blogs?.map((blog: any) => (
             <BlogCard key={blog._id?.toString()} blog={blog} />
